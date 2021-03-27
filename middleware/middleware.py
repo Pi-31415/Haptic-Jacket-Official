c83b1e6f7e2218a27a90ee81d3319f8d1e781e4b
Pi <paingthetko.pi@gmail.com>
2021-03-27 13:56:33 +0630

    middleware

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -7,14 +7,33 @@ Dependencies : socket
 
 
 import socket
+from time import sleep
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 33333
 
-def activate_motor(motor_id):
-    COMMAND = motor_id
+def send_UDP_message(message):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet  # UDP
-    sock.sendto(COMMAND, (UDP_IP, UDP_PORT))
-    print("message: %s" % COMMAND)
+    sock.sendto(message, (UDP_IP, UDP_PORT))
 
-activate_motor(1)
+def activate_motor(motor_id):
+    COMMAND = bytes(str(motor_id),'utf-8')
+    send_UDP_message(COMMAND)
+    print("Motor : %s Activated" % COMMAND)
+
+def stop_all_motors():
+    COMMAND = bytes('0','utf-8')
+    send_UDP_message(COMMAND)
+    print("All Motors Stopped")
+
+def continuous():
+    x = 1
+    while True:
+        stop_all_motors()
+        activate_motor(x)
+        sleep(0.1)
+        x += 1
+        if(x>=18):
+            x=1
+
+continuous()
