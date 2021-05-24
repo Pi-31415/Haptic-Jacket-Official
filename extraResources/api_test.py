afe3534aab59b922cd2f5be56aa3e0e2ab23b1f3
Pi <paingthetko.pi@gmail.com>
2021-05-24 23:55:12 +0630

    .

--- extraResources/api_test.py
+++ extraResources/api_test.py
@@ -3,12 +3,11 @@ import hapticAPI
 hapticAPI.initiate_config()
 hapticAPI.show_modules()
 
-# activate_motor(module_id,intensity,duration) Intensity = 0 to 100, 
+# activate_motor(module_id,intensity,duration) Intensity = 0 to 100,
 # Duration in milliseconds
 # hapticAPI.delay(Duration in milliseconds)
 
 
-hapticAPI.activate_motor(1,90,5000)
-hapticAPI.activate_motor(1,100,5000)
-hapticAPI.activate_motor(1,80,5000)
-
+hapticAPI.activate_motor(1, 90, 5000)
+hapticAPI.activate_motor(1, 100, 5000)
+hapticAPI.activate_motor(1, 80, 5000)
