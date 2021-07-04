900eefe04bac3d544229926ebdc5323822fd6ed6
Pi <paingthetko.pi@gmail.com>
2021-07-04 23:00:12 +0630

    .

--- www/scripts/sketch.js
+++ www/scripts/sketch.js
@@ -573,8 +573,20 @@ class VibrationMotor {
 
 function launch_hand_control() {
 
-  var spawn = require("child_process").spawn;
-  var process = spawn('python3', ["./API/handdetect.py"]);
+
+  const { spawn } = require('child_process');
+  const ls = spawn('python3', ["\"./API/handdetect.py\""]);
+  ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  ls.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
 }
 
 function scan_modules() {
