41fc7bb537be925f7fdbebd989e019778bc386c1
Pi <paingthetko.pi@gmail.com>
2021-07-04 17:57:39 +0630

    .

--- macbuild.sh
+++ macbuild.sh
@@ -1,3 +1,6 @@
 sudo electron-packager . --overwrite --platform=darwin --arch=x64 --icon=assets/icons/icon.icns --prune=true --out=release-builds
 sudo cp -R ./extraResources/ ./release-builds/Haptic-Jacket-Controller-darwin-x64
 sudo chmod -R 777 release-builds
+rm release-builds/Haptic-Jacket-Controller-darwin-x64/LICENSES.chromium.html
+rm release-builds/Haptic-Jacket-Controller-darwin-x64/LICENSE
+rm release-builds/Haptic-Jacket-Controller-darwin-x64/version
